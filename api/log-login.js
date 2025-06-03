0aaa4cd060c352695483d28af135208404c62bf0
searchfile <5g88.offical@gmail.com>
2025-06-03 14:59:15 +0800

    git add . git commit -m "fix: convert to CommonJS for Vercel support" git push

--- api/log-login.js
+++ api/log-login.js
@@ -2,7 +2,7 @@ const axios = require('axios');
 
 module.exports = async (req, res) => {
   if (req.method !== 'POST') {
-    return res.status(405).json({ error: 'Method not allowed' });
+    return res.status(405).json({ error: 'Method not allowed' }); 
   }
 
   const { name, email } = req.body;
